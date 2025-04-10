e4602a3548ebfba99d970e15122c6ca8edcc32e8
Roberto Pineda <pin.r.30@gmail.com>
2025-04-10 11:47:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/components/icons.ts:36:7
    > Parsing ecmascript source code failed
    >   34 |   record: (props: React.SVGProps<SVGSVGElement>) => (
    >   35 |     <svg
    > > 36 |       xmlns="http://www.w3.org/2000/svg"
    >      |       ^^^^^
    >   37 |       width="24"
    >   38 |       height="24"
    >   39 |       viewBox="0 0 24 24"
    >
    > Expected '>', got 'xmlns'
    >
    >

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -31,21 +31,20 @@ const Icons = {
   trash: Trash,
   user: User,
   pause: Pause,
-  recording: (props: React.SVGProps<SVGSVGElement>) => (
+  record: (props: React.SVGProps<SVGSVGElement>) => (
     <svg
+      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      fill="none"
+      fill="currentColor"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="0"
       strokeLinecap="round"
       strokeLinejoin="round"
-      {...props}
-    >
-      <rect width="16" height="16" x="4" y="4" rx="8" />
-    </svg>
+
+    />
   ),
 };
 
